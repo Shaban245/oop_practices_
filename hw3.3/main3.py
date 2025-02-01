58f8d5037c2695af2f6a86001888691baeadd959
shaban <shaban.elaev@mail.ru>
2025-02-01 15:15:33 +0300

    feat class Robot

---
+++ hw3.3/main3.py
@@ -0,0 +1,46 @@
+class Robot:
+    number:int
+    model: str
+    task: str
+    battery_charge: int
+    is_work: bool
+
+    def __init__(self,number, model, task, battery_charge, is_work = None):
+        self.__number = number
+        self.__model = model
+        self.__task = task
+        self.__battery_charge = battery_charge
+        self.__is_work = is_work
+
+    def get_number(self):
+        return self.__number
+
+    def get_model(self):
+        return self.__model
+
+    def get_task(self):
+        return self.__task
+
+    def get_battery_charge(self):
+        return self.__battery_charge
+
+    def get_is_work(self):
+        return self.__is_work
+
+    def change_charge(self, charge):
+        self.__battery_charge = charge
+
+    def start_work(self):
+        self.__is_work = True
+
+    def end_work(self):
+        self.__is_work = False
+
+    def set_task(self, new_task):
+        self.__task = new_task
+
+    def set_model(self, new_model):
+        self.__model = new_model
+
+    def __str__(self):
+        print(f"состояние заряда батареи : {self.__battery_charge}")
